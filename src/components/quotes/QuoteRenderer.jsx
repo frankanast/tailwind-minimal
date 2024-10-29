58b6a35162e06c507ba9d2af508026a0287df38b
franc <francesco.anastasio93@gmail.com>
2024-10-29 21:11:50 +0100

    Experiments for implementation for new email generation endpoint (2)

--- src/components/quotes/QuoteRenderer.jsx
+++ src/components/quotes/QuoteRenderer.jsx
@@ -13,7 +13,7 @@ export default function QuoteRenderer() {
         if (parsedData) {
             const sendParsedData = async () => {
                 try {
-                    const response = await fetch('/generate-html', {
+                    const response = await fetch('https://www.programmino-be.onrender.com/generate-html', {
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify({ content: parsedData })
